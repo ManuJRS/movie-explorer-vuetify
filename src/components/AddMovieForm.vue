a71a5920d8bfd8cec9e1fdf17bcd1e45aa246667
ManuJRS <manueljesusrejonsantana@gmail.com>
2025-11-13 14:34:23 -0600

    fix(form): :white_check_mark: add form validations and repair fetch process
    Implemented input validations and Fixed the fetch

--- src/components/AddMovieForm.vue
+++ src/components/AddMovieForm.vue
@@ -1,22 +1,33 @@
 <script setup>
 import { ref } from 'vue';
 import { useMoviesStore } from '../stores/movies';
+const isFormValid = ref(false);
 const moviesStore = useMoviesStore()
 const title = ref('');
 const year = ref('');
 const image = ref('');
+const form = ref(null);
+const required = (value) => !!value || 'Este campo es requerido';
+const isYear = (value) => 
+     (value && value > 1800 && value < 2100) || 'Ingrese un año valido';
 
-function handleSubmit() {
+async function handleSubmit() {
+    const Valid = await form.value.validate();
+    if (!Valid) return;
+
     moviesStore.addMovie({
         id: Date.now(),
         title: title.value,
-        year: year.value,
+        year: Number(year.value),
         image: image.value
     });
+
 console.log('Películas en el store:', moviesStore.movies)
     title.value = '';
     year.value = '';
     image.value = '';
+    form.value.reset();
+    form.value.resetValidation();
 }
 </script>
 
@@ -24,11 +35,35 @@ console.log('Películas en el store:', moviesStore.movies)
     <v-card>
         <v-card-title>Aqui puedes agregar tus peliculas</v-card-title>
         <v-card-text>
-            <v-form @submit.prevent="handleSubmit">
-                <v-text-field v-model="title" label="Titulo de la pelicula" required></v-text-field>
-                <v-text-field v-model="year" label="Año de lanzamiento" required></v-text-field>
-                <v-text-field v-model="image" label="URL de la imagen" required></v-text-field>
-                <v-btn color="primary" type="submit">Agregar Pelicula</v-btn>
+            <v-form
+            v-model="isFormValid"
+            ref="form"
+            @submit.prevent="handleSubmit">
+                <v-text-field 
+                v-model="title" 
+                label="Titulo de la pelicula"
+               :rules="[required]"
+                required
+                ></v-text-field>
+                <v-text-field 
+                v-model="year" 
+                label="Año de lanzamiento"
+                :rules="[required,isYear]"
+                required
+                ></v-text-field>
+                <v-text-field 
+                v-model="image" 
+                label="URL de la imagen"
+                :rules="[required]"
+                required
+                ></v-text-field>
+                <v-btn
+                class="mt-5" 
+                :disabled="!isFormValid"
+                color="primary" 
+                type="submit"
+                block
+                >Agregar Pelicula</v-btn>
             </v-form>
         </v-card-text>
     </v-card>
