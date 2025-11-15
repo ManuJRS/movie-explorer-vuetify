c584f47f97d78249b2290e5578b77d34ba10375a
Manu <manueljesusrejonsantana@gmail.com>
2025-11-14 20:05:40 -0600

    fix(bug): :art: fix bug
    i fix a bug in staging develop preview

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,7 +9,7 @@ class="footer"
 elevation="0"
 >
     <v-container class="text-center py-4">
-        <span class="footer-text">Desarrollado por <strong>ManuDev</strong> - {{ year }} - Sitio de staging</span>
+        <span class="footer-text">Desarrollado por <strong>ManuDev</strong> - {{ year }}</span>
     </v-container>
 </v-footer>
 </template>
